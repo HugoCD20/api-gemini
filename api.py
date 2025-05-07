8da238597710328df6114c10092a480eea333158
Hugo David Nogueda Hernandez <cdhugo9@gmail.com>
2025-05-06 22:57:04 -0600

    todo guardado

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS  
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])
 api = Api(app)
 
-todos = {}
 
 import main as resourcest
 
--- main/Model.py
+++ main/Model.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import time
+import google.genai.errors
 
 load_dotenv()
 
@@ -9,12 +11,25 @@ chat_session = client.chats.create(model="gemini-2.0-flash")
 
 
 def texto(texto):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=[texto]
-    )
-    return response.text
+     for _ in range(5):  
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=[texto]
+            )
+            return response.text
+        except google.genai.errors.ServerError as e:
+            print("Error del servidor, reintentando en 5 segundos...")
+            time.sleep(5)
+    
+    
 
 def chat(texto):
-    response = chat_session.send_message(texto)
-    return response.text
+    for _ in range(5):  
+        try:
+            response = chat_session.send_message(texto)
+            return response.text
+        except google.genai.errors.ServerError as e:
+            print("Error del servidor, reintentando en 5 segundos...")
+            time.sleep(5)
+    
